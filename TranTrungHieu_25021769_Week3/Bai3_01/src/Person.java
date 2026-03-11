1573313ea97b22c324537cbe01e0258480f4e033
trunghieutran632007-code <trunghieu.tran632007@gmail.com>
2026-03-11 15:53:29 +0700

    abc

--- TranTrungHieu_25021769_Week3/Bai3_01/src/Person.java
+++ TranTrungHieu_25021769_Week3/Bai3_01/src/Person.java
@@ -3,10 +3,15 @@ package Bai3_01.src;
 public class Person {
     String name;
     String dob;
-
+/* 
     public Person(){
         System.out.println("1. Person is created");
         
     }
+*/
 
+//Sua thanh constructor co tham so
+    public Person(String name) {
+        this.name = name;
+    }
 }
---
+++ TranTrungHieu_25021769_Week3/Bai3_02/src/Main.java
@@ -0,0 +1,5 @@
+package Bai3_02.src;
+
+public class Main {
+
+}
