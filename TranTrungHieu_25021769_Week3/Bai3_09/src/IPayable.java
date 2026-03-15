356552f8dab14157b79f0f97ea2b4870a5176621
trunghieutran632007-code <trunghieu.tran632007@gmail.com>
2026-03-15 23:35:18 +0700

    Bai 9

---
+++ TranTrungHieu_25021769_Week3/Bai3_09/src/IPayable.java
@@ -0,0 +1,6 @@
+package Bai3_09.src;
+
+public interface IPayable {
+    public double getPaymentAmount();
+
+}
---
+++ TranTrungHieu_25021769_Week3/Bai3_09/src/Staff.java
@@ -0,0 +1,7 @@
+package Bai3_09.src;
+
+public abstract class Staff {
+    public String id;
+    public String name;
+
+}
