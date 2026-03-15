bfc78e122903751933352df0a8755cbd27991951
trunghieutran632007-code <trunghieu.tran632007@gmail.com>
2026-03-15 22:29:34 +0700

    Update Robot.java

--- TranTrungHieu_25021769_Week3/Bai3_08/src/Robot.java
+++ TranTrungHieu_25021769_Week3/Bai3_08/src/Robot.java
@@ -19,23 +19,73 @@ public abstract class Robot {
         System.out.println(modelName);
     }
 
+    public String getModelName() {
+        return modelName;
+    }
+
     public abstract void performMainTask();
 }
 
 class DroneRobot extends Robot implements Flyable, GPS {
+    public DroneRobot (int id, String modelName) {
+        super(id, modelName);
+    }
+    @Override
+    public void performMainTask() {
+        System.out.println(getModelName() + " performing main task");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(getModelName() + " flying");
+    }
+
+    @Override
+    public void getCoordinates() {
+        System.out.println(getModelName() + " getting coordinates");
+    }
 
 
 }
 
 class FishRobot extends Robot implements Swimmable {
+    public FishRobot(int id, String modelName) {
+        super(id, modelName);
+    }
 
+    @Override
+    public void performMainTask() {
+        System.out.println(getModelName() + " performing main task");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println(getModelName() + " swimming");
+    }
 }
 
 class AmphibiousRobot extends Robot implements Flyable, GPS, Swimmable {
+    public AmphibiousRobot (int id, String modeName) {
+        super(id, modeName);
+    }
+
     @Override
     public void performMainTask() {
-        
-    
-        
+        System.out.println(getModelName() + " performing main task");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(getModelName() + " flying");
+    }
+
+    @Override
+    public void getCoordinates() {
+        System.out.println(getModelName() + " getting coordinates");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println(getModelName() + " swimming");
     }
 }
