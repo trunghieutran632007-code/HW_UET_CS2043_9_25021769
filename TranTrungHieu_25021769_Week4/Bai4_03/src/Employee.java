af77b3c7b93755dfedf2a223906218b301db11c7
trunghieutran632007-code <trunghieu.tran632007@gmail.com>
2026-03-21 18:58:15 +0700

    kl;j;jk

--- TranTrungHieu_25021769_Week4/Bai4_03/src/Employee.java
+++ TranTrungHieu_25021769_Week4/Bai4_03/src/Employee.java
@@ -5,6 +5,46 @@ public abstract class Employee implements IWorkable {
     String name;
     double baseSalary;
 //Employee khong can implements void work(), co the de class con thuc hien dieu do
+
+    public Employee(String id, String name, double baseSalary) {
+        this.id = id;
+        this.name = name;
+        this.baseSalary = baseSalary;
+    }
+
+    public abstract double calculatePay();
 }
 
-class OfficeWorker
+class OfficeWorker extends Employee {
+    public OfficeWorker(String id, String name, double baseSalary) {
+        super(id, name, baseSalary);
+    }
+
+    @Override
+    public double calculatePay() {
+        return baseSalary;
+    }
+
+    @Override
+    public void work() {
+        System.out.println("Soan thao van ban");
+    }
+}
+
+class Technician extends Employee {
+    double overtimeHours;
+    public Technician(String id, String name, double baseSalary, double overtimeHours) {
+        super(id, name, baseSalary);
+        this.overtimeHours = overtimeHours;
+    }
+
+    @Override
+    public double calculatePay() {
+        return baseSalary + overtimeHours * 20000;
+    }
+
+    @Override
+    public void work() {
+        System.out.println("Lap dat thiet bi");
+    }
+}
--- TranTrungHieu_25021769_Week4/Bai4_03/src/Main.java
+++ TranTrungHieu_25021769_Week4/Bai4_03/src/Main.java
@@ -1,7 +1,41 @@
 package Bai4_03.src;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap so luong nhan vien: ");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+
+        Employee[] employees = new Employee[n];
+
+        for (int i = 0; i < n; i++) {
+            String info = scanner.nextLine();
+            String[] parts = info.strip().split("\\s+");
+            if (parts[0].equalsIgnoreCase("O")) {
+                String id = parts[1];
+                String name = parts[2];
+                double baseSalary = Double.parseDouble(parts[3]);
+                employees[i] = new OfficeWorker(id, name, baseSalary);
+            } else if (parts[0].equalsIgnoreCase("T")) {
+                String id = parts[1];
+                String name = parts[2];
+                double baseSalary = Double.parseDouble(parts[3]);
+                double overtimeHours = Double.parseDouble(parts[4]);
+                employees[i] = new Technician(id, name, baseSalary, overtimeHours);
+            }
+        }
+
+        double totalPay = 0;
+        for (int i = 0; i < n; i++) {
+            System.out.println(employees[i].name + " - Pay: " + employees[i].calculatePay());
+            employees[i].work();
+            System.out.println();
+            totalPay = totalPay + employees[i].calculatePay();
+        }
+        System.out.println("Total Pay: " + totalPay);
         
     }
 
