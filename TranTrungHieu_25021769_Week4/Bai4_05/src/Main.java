25cd0b655f75fd9d77881f6fb6018edf75e32105
trunghieutran632007-code <trunghieu.tran632007@gmail.com>
2026-03-21 13:48:38 +0000

    xong bai 5

---
+++ TranTrungHieu_25021769_Week4/Bai4_05/src/Main.java
@@ -0,0 +1,12 @@
+package Bai4_05.src;
+
+public class Main {
+    public static void main(String[] args) {
+        Pair<String, Integer> p1 = new Pair<>("Tuoi", 20);
+        Pair<String, String> p2 = new Pair<>("Mã SV", "SV001");
+        Pair<Integer, Double> p3 = new Pair<>(105, 21.5);
+        //Pair<Integer, Double> p4 = new Pair<>("105", 21.5);
+        //Bao loi: Cannot infer type arguments for Pair<>
+    }
+
+}
