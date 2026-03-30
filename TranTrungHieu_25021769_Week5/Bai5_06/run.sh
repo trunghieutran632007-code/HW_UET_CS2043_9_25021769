059a8eaf512298c081bf3fac46eb8e12c9a11dae
trunghieutran632007-code <trunghieu.tran632007@gmail.com>
2026-03-30 21:17:44 +0700

    done run.sh w5

---
+++ TranTrungHieu_25021769_Week5/Bai5_06/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ROOT_DIR=$(pwd)
+SRC_DIR="$ROOT_DIR/src"
+BUILD_DIR="$ROOT_DIR/build"
+LIB_DIR="$ROOT_DIR/lib"
+
+mkdir -p "$BUILD_DIR"
+
+# Kiểm tra có thư viện không
+if [ -d "$LIB_DIR" ] && [ "$(ls -A $LIB_DIR/*.jar 2>/dev/null)" ]; then
+    CLASSPATH=$(find "$LIB_DIR" -name "*.jar" | tr '\n' ':')
+    javac -d "$BUILD_DIR" -cp "$CLASSPATH" "$SRC_DIR"/*.java
+    java -cp "$BUILD_DIR:$CLASSPATH" Bai5_06.src.Main
+else
+    javac -d "$BUILD_DIR" "$SRC_DIR"/*.java
+    java -cp "$BUILD_DIR" Bai5_06.src.Main
+fi
