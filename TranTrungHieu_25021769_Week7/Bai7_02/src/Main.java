b210fa0c1253311de0876a6ad646390abf8980a0
trunghieutran632007-code <trunghieu.tran632007@gmail.com>
2026-04-10 05:01:43 +0000

    àdasd

--- TranTrungHieu_25021769_Week7/Bai7_02/src/Main.java
+++ TranTrungHieu_25021769_Week7/Bai7_02/src/Main.java
@@ -1,7 +1,20 @@
 package Bai7_02.src;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Nhap so luong phan tu: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            sc.nextLine();
+        }
     }
 
 }
