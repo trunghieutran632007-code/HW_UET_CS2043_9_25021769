a918d50370153eb565e5f3b706b48d0c37fb91b8
trunghieutran632007-code <trunghieu.tran632007@gmail.com>
2026-05-03 21:33:45 +0700

    fix: resolve checkstyle violations

--- TranTrungHieu_25021769_Week9/Bai9_07/src/BuggyDemo.java
+++ TranTrungHieu_25021769_Week9/Bai9_07/src/BuggyDemo.java
@@ -1,6 +1,8 @@
-public class BuggyDemo{
-    public static void main(String[] args){
-    int    x=10;
-    if(x>5){System.out.println("big");}
+public class BuggyDemo {
+    public static void main(String[] args) {
+        int x = 10;
+        if (x > 5) {
+            System.out.println("big");
+        }
     }
 }
